68e014ea566776793a496a1113a0402b0aeea9d0
martin <kayondo@snu.ac.kr>
2023-06-03 23:27:53 +0900

    defaulting to mimalloc

--- src/lib.rs
+++ src/lib.rs
@@ -19,17 +19,17 @@ unsafe impl GlobalAlloc for MetaSafeAlloc {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         if layout.align() < MIN_ALIGN {
-            return ffi::mi_malloc_aligned(layout.size(), MIN_ALIGN) as *mut u8;
+            return ffi::mi_malloc(layout.size()) as *mut u8;
         }
-        ffi::mi_malloc_aligned(layout.size(), layout.align()) as *mut u8
+        ffi::mi_malloc(layout.size()) as *mut u8
     }
 
     #[inline]
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
         let ptr = if layout.align() < MIN_ALIGN {
-            ffi::mi_malloc_aligned(layout.size(), MIN_ALIGN)
+            ffi::mi_malloc(layout.size())
         } else {
-            ffi::mi_malloc_aligned(layout.size(), layout.align())
+            ffi::mi_malloc(layout.size())
         };
 
         if !ptr.is_null() {
@@ -39,14 +39,14 @@ unsafe impl GlobalAlloc for MetaSafeAlloc {
     }
 
     #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        ffi::mi_free_aligned(ptr as *mut c_void, layout.align())
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        ffi::mi_free(ptr as *mut c_void)
     }
 
     #[inline]
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        let align = if layout.align() < MIN_ALIGN { MIN_ALIGN } else {layout.align()};
-        ffi::mi_realloc_aligned(ptr as *mut c_void, new_size, align) as *mut u8
+        let _align = if layout.align() < MIN_ALIGN { MIN_ALIGN } else {layout.align()};
+        ffi::mi_realloc(ptr as *mut c_void, new_size) as *mut u8
     }
 }
 
