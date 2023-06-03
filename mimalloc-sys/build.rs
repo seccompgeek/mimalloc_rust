13fc0048828426aab6cbca2842aee75dcc215df6
martin <kayondo@snu.ac.kr>
2023-06-03 23:04:03 +0900

    fix reversed build bug

--- mimalloc-sys/build.rs
+++ mimalloc-sys/build.rs
@@ -9,11 +9,11 @@ fn main(){
     if env::var_os("CARGO_FEATURE_DEBUG").is_some()
         || (env::var_os("CARGO_FEATURE_DEBUG_IN_DEBUG").is_some() && cfg!(debug_assertions))
     {
-        dst.define("CMAKE_BUILD_TYPE", "Release");
-    } else {
-        // Remove heavy debug assertions etc
         dst.define("CMAKE_BUILD_TYPE", "Debug");
         out_name = "mimalloc-debug"
+    } else {
+        // Remove heavy debug assertions etc
+        dst.define("CMAKE_BUILD_TYPE", "Release");
     }
 
     let dst = dst.build();
